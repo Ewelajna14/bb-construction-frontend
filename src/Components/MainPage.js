a7cf419753f61f6ce868283af807b4da7d63c406
Ewelina <ekowalczyk1994@gmail.com>
2022-06-07 14:51:25 -0500

    new components

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -5,7 +5,7 @@ function MainPage(){
         <div>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                   <h3>Services</h3>
+                   <div className='main-title'>Services</div>
                 </Grid>
 
             </Grid>
