5b91de499d2b9fca19d7ef0f3ad827f5bb32a786
Ewelina <ekowalczyk1994@gmail.com>
2022-06-08 14:24:59 -0500

    form structure

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,11 +1,22 @@
 
 function ContactForm(){
     return(
-        <div>
+      
             <form>
-
+                <div className="form-group">
+                    <input type="text" name="client_name"></input>
+                    <label for="client_name">Your Name</label>
+                 </div>
+                 <div className="form-group">
+                    <input type="email" name="client_email"></input>
+                    <label for="client_email">Your email</label>
+                 </div>
+                 <div className="form-group">
+                    <textarea name="message" rows="6"></textarea>
+                    <label for="message">Message</label>
+                 </div>
             </form>
-        </div>
+       
     )
 }
 
