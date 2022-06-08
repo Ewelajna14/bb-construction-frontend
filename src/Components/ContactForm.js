d16398e570f51b992351132e0469530eecfd06cd
Ewelina <ekowalczyk1994@gmail.com>
2022-06-08 15:12:57 -0500

    form icons

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -7,15 +7,15 @@ function ContactForm(){
                 <h2>Contact Us</h2>
                 <div className="form-group">
                     <input type="text" name="client_name"></input>
-                    <label for="client_name">Your Name</label>
+                    <label for="client_name"><i class="fa-solid fa-user-pen"></i>Your Name</label>
                  </div>
                  <div className="form-group">
                     <input type="email" name="client_email"></input>
-                    <label for="client_email">Your email</label>
+                    <label for="client_email"><i class="fa-solid fa-envelope"></i>Your email</label>
                  </div>
                  <div className="form-group">
                     <textarea name="message" rows="6"></textarea>
-                    <label for="message">Message</label>
+                    <label for="message"><i class="fa-solid fa-message"></i>Message</label>
                  </div>
                  <button type="submit">Send</button>
             </form>
