cf436c8dee52e948f9b43956f2dcf969f68a6025
Ewelina <ekowalczyk1994@gmail.com>
2022-06-13 11:46:39 -0500

    new services

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer} from 'react-leaflet'
 function Map(){
     return(
         <div>
-            <MapContainer center={[41.9919, -87.8242]} zoom={11} scrollWheelZoom={false}>
+            <MapContainer center={[41.9919, -87.8242]} zoom={10} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
