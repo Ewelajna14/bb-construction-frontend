03c43b7b3507df951904a265197e71088341b436
Ewelina <ekowalczyk1994@gmail.com>
2022-06-13 12:54:03 -0500

    background photo

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,15 +1,9 @@
-import {Grid} from '@mui/material'
+import './MainPage.css'
 
 function MainPage(){
     return(
-        <div>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                   <div className='main-title'>Services</div>
-                </Grid>
-
-            </Grid>
-
+        <div className='bg-photo'>
+          <div className='title'>Double B Construction</div>
         </div>
     )
 }
