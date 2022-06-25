61d72dab072c91a70c7a0c248c69a0db56d199e9
Ewelina <ekowalczyk1994@gmail.com>
2022-06-25 13:57:48 -0500

    home component

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import MainPage from './MainPage';
+import ContactForm from './ContactForm';
+import Map from './Map'
+import Services from './Services';
+
+function Home(){
+    return(
+        <div>
+            <MainPage/>
+            <Services/>
+            <ContactForm/>
+            <Map/>
+        </div>
+    )
+}
+
+export default Home
