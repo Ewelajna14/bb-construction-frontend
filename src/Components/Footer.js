64258ef65b893163e9ea6887a425b235e2691849
Ewelina <ekowalczyk1994@gmail.com>
2022-07-23 12:56:07 -0500

    footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,39 @@
 import './Footer.css'
+
 function Footer(){
     return(
         <div className="main-footer">
-          <div className="container"></div>
-          <div className="row"></div>
-          THIS IS FOOTER
+          <div className="container">
+             <div className="row">
+               {/*Column1 */}
+               <div className='col'>
+                <h4>BB Construction</h4>
+                <ul className='list-unstyled'>
+                   <li>773-xxx-xxx</li>
+                   <li>Chicago</li>
+                   <li>Suburbs</li>
+                </ul>
+               </div>
+               {/*Column2 */}
+               <div className='col'>
+               <h4>BB Construction</h4>
+                <ul className='list-unstyled'>
+                   <li>773-xxx-xxx</li>
+                   <li>Chicago</li>
+                   <li>Suburbs</li>
+                </ul>
+               </div>
+               {/*Column3 */}
+               <div className='col'>
+               <h4>BB Construction</h4>
+                <ul className='list-unstyled'>
+                   <li>773-xxx-xxx</li>
+                   <li>Chicago</li>
+                   <li>Suburbs</li>
+                </ul>
+               </div>
+             </div>
+          </div>
         </div>
     )
 }
