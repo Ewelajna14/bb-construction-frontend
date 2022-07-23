43be0b6317dca74549eab6fbf71bb05d54555eb6
Ewelina <ekowalczyk1994@gmail.com>
2022-07-23 12:50:01 -0500

    structure footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,9 @@
+import './Footer.css'
 function Footer(){
     return(
-        <div>
+        <div className="main-footer">
+          <div className="container"></div>
+          <div className="row"></div>
           THIS IS FOOTER
         </div>
     )
