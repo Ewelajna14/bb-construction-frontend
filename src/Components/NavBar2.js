e317522b2f30f6c77b55e8ec16e410d885af46ff
Ewelina <ekowalczyk1994@gmail.com>
2022-11-12 17:59:31 -0600

    navbar styling

--- src/Components/NavBar2.js
+++ src/Components/NavBar2.js
@@ -1,16 +1,27 @@
 import './NavBar2.css'
+import {useState} from 'react'
 
 function NavBar2(){
+
+    const [click, setClick] = useState(false)
+
 return(
    <>
    <nav>
     <a href="/">LOGO</a>
     <div>
         <ul id="navbar">
-            <li><a href="/">Home</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/">Services</a></li>
+            <li><a className='active'
+            href="/">Home</a></li>
+            <li><a 
+            href="/contact">Contact</a></li>
+            <li><a 
+            href="/">Services</a></li>
         </ul>
+    </div>
+
+    <div id="mobile">
+       <i id="bar" className={click? "fas fa-times":" fas fa-bars" } onClick={()=>setClick(!click)}></i>
     </div>
    </nav>
    </>
