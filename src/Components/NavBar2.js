0303546f11d13e4788d9de3448a74b008565ae91
Ewelina <ekowalczyk1994@gmail.com>
2022-11-12 17:34:40 -0600

    new  navbar

---
+++ src/Components/NavBar2.js
@@ -0,0 +1,20 @@
+import './NavBar2.css'
+
+function NavBar2(){
+return(
+   <>
+   <nav>
+    <a href="/">LOGO</a>
+    <div>
+        <ul id="navbar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/">Services</a></li>
+        </ul>
+    </div>
+   </nav>
+   </>
+)
+}
+
+export default NavBar2
